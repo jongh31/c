a4ac4baf6d2d40586575da45bcbbeb7f1abfd779
jongh31 <hyouk3004@naver.com>
2022-09-21 02:28:00 +0900

    Add Text File [hw2.c]

---
+++ hw2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+	float num1;
+	printf("Please enter kilometers:");
+	scanf("%f", &num1);
+	float num2;
+	num2 = num1 / 1.609f;
+	printf("%.1f km is equal to %.1f miles.", num1, num2);
+	return 0;
+}
